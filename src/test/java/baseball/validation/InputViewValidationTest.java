ed17c1b2755bbf0980818b679b0fa89c7acb2964
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 18:36:25 +0900

    test(validation: InputViewValidation): 재시작 여부 명령어 올바른지 검증기능 테스트

--- src/test/java/baseball/validation/InputViewValidationTest.java
+++ src/test/java/baseball/validation/InputViewValidationTest.java
@@ -3,6 +3,7 @@ package baseball.validation;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static baseball.constant.ErrorConstant.ERROR_NOT_VALID_GAME_COMMAND;
 import static org.assertj.core.api.Assertions.*;
 
 class InputViewValidationTest {
@@ -11,13 +12,28 @@ class InputViewValidationTest {
     @ValueSource(strings = {"23", "3uh", "i", " ", "", "112", "0", "012"})
     void 입력받은_숫자_예외_검증(String input) {
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> InputViewValidation.checkInputValid(input));
+                .isThrownBy(() -> InputViewValidation.checkNumberValid(input));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"123", "347", "789", "472"})
     void 입력_숫자_올바른경우_검증(String input) {
-        assertThatCode(() -> InputViewValidation.checkInputValid(input))
+        assertThatCode(() -> InputViewValidation.checkNumberValid(input))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"22", "h", " ", "",})
+    void 재시작_여부_명령어_예외_검증(String input) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> InputViewValidation.checkCommandValid(input))
+                .withMessageStartingWith(ERROR_NOT_VALID_GAME_COMMAND);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "1"})
+    void 재시작_여부_명령어_올바른경우_검증(String input) {
+        assertThatCode(() -> InputViewValidation.checkCommandValid(input))
                 .doesNotThrowAnyException();
     }
 }

378a5a12eda4df84b579de932a4fd35d94ce11df
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 17:30:27 +0900

    feat(view: InputView): 유저로부터 값을 입력받는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,14 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.constant.MessageConstant.MESSAGE_INPUT_NUMBER;
+
 public class InputView {
+
+    public String inputNumber() {
+        System.out.print(MESSAGE_INPUT_NUMBER);
+        String userNumber = Console.readLine();
+        return userNumber;
+    }
 }

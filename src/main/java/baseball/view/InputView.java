4ba0e390bd840b09941ab8659cb7437d4dd9feff
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 18:25:15 +0900

    feat(view: InputView): 게임 재시작 여부를 입력받는다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,14 +3,21 @@ package baseball.view;
 import baseball.validation.InputViewValidation;
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.constant.MessageConstant.MESSAGE_INPUT_EXIT;
 import static baseball.constant.MessageConstant.MESSAGE_INPUT_NUMBER;
 
 public class InputView {
 
-    public String inputNumber() {
+    public String readUserNumber() {
         System.out.print(MESSAGE_INPUT_NUMBER);
-        String input = Console.readLine();
-        InputViewValidation.checkInputValid(input);
-        return input;
+        String userNumber = Console.readLine();
+        InputViewValidation.checkInputValid(userNumber);
+        return userNumber;
+    }
+
+    public String readGameCommand() {
+        System.out.println(MESSAGE_INPUT_EXIT);
+        String command = Console.readLine();
+        return command;
     }
 }

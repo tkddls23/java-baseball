ff7ee0927eca1b29da4e08dd1ce6a7c0959cc1c0
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 19:31:52 +0900

    feat(domain: Computer): 컴퓨터의 수를 생성한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import baseball.view.InputView;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,23 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.constant.GameConstant.*;
+
 public class Computer {
+    private List<Integer> computerNumbers;
+
+    public void generateRandomNumber() {
+        this.computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < CNT_NUMBER) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+            if (!computerNumbers.contains(randomNumber)) {
+                this.computerNumbers.add(randomNumber);
+            }
+        }
+    }
 }

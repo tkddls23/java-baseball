03e54883fc17ca3affa3079c96a9d6a0889e83c9
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 17:24:20 +0900

    feat: 입력 출력 상수 생성

---
+++ src/main/java/baseball/constant/MessageConstant.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class MessageConstant {
+
+    public static final String MESSAGE_INPUT_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String MESSAGE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+
+    public static final String MESSAGE_OUTPUT_START =  "숫자 야구 게임을 시작합니다.";
+    public static final String MESSAGE_OUTPUT_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}

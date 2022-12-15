fd6ebf9bc2baaa3d4e39f90bedcc808a36584258
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 20:09:03 +0900

    feat(domain: Computer): 유저의 수와 컴퓨터의 수에 대한 결과를 판단 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -9,6 +9,7 @@ import static baseball.constant.GameConstant.*;
 
 public class Computer {
     private List<Integer> computerNumbers;
+    private List<Integer> scores;
 
     public List<Integer> getComputerNumbers() {
         return this.computerNumbers;
@@ -23,5 +24,47 @@ public class Computer {
                 this.computerNumbers.add(randomNumber);
             }
         }
+    }
+
+    public String getHint(String userNumber) {
+        calculateScore(userNumber);
+        StringBuilder hint = new StringBuilder();
+        if (scores.get(0) == 0 && scores.get(1) == 0) {
+            return NOTHING;
+        }
+        if (scores.get(0) > 0) {
+            hint.append(scores.get(0)).append(BALL);
+        }
+        if (scores.get(1) > 0) {
+            hint.append(scores.get(1)).append(STRIKE);
+        }
+        return hint.toString();
+    }
+
+    private void calculateScore(String userNumber) {
+        int ballCount = getBallCount(userNumber);
+        int strikeCount = getStrikeCount(userNumber);
+
+        this.scores = List.of(ballCount - strikeCount, strikeCount);
+    }
+
+    private int getBallCount(String userNumber) {
+        int ballCount = 0;
+
+        for (int i = 0; i < CNT_NUMBER; i++) {
+            if (computerNumbers.contains(Character.getNumericValue(userNumber.charAt(i))))
+                ballCount ++;
+        }
+        return ballCount;
+    }
+
+    private int getStrikeCount(String userNumber) {
+        int strikeCount = 0;
+
+        for (int i = 0; i < CNT_NUMBER; i++) {
+            if (computerNumbers.get(i) == (Character.getNumericValue(userNumber.charAt(i))))
+                strikeCount += 1;
+        }
+        return strikeCount;
     }
 }

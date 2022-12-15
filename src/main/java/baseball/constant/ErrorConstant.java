5d6af823e18742071be9aa4b83804e8bdcf6fb14
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 17:48:05 +0900

    feat(validation: InputViewValidation): 입력한 수가 세자리인지 검증 구현

---
+++ src/main/java/baseball/constant/ErrorConstant.java
@@ -0,0 +1,8 @@
+package baseball.constant;
+
+public class ErrorConstant {
+
+    public static final String ERROR_NON_INTEGER = "[ERROR] 숫자가 아닙니다.";
+    public static final String ERROR_NOT_VALID_LENGTH = "[ERROR] 3자리가 아닙니다.";
+
+}
--- src/main/java/baseball/validation/InputViewValidation.java
+++ src/main/java/baseball/validation/InputViewValidation.java
@@ -1,11 +1,20 @@
 package baseball.validation;
 
+import static baseball.constant.ErrorConstant.ERROR_NON_INTEGER;
+import static baseball.constant.ErrorConstant.ERROR_NOT_VALID_LENGTH;
+
 public class InputViewValidation {
 
     private void checkInputDigit(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i)))
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException(ERROR_NON_INTEGER);
+        }
+    }
+
+    private void checkInputLengthThree(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException(ERROR_NOT_VALID_LENGTH);
         }
     }
 }
